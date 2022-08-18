e7819f973e2ed8bc7c2a86e934a5217749a3c1e4
ashdederich <ashley.dederich@utsouthwestern.edu>
2022-08-18 15:22:50 -0600

    adjusted file to work with nextflow script

--- Preparing_For_CellProfiler/generate_illumCsv.sh
+++ Preparing_For_CellProfiler/generate_illumCsv.sh
@@ -1,27 +1,25 @@
 #!/bin/bash
-dir=$1
 
-cd $dir
+loaddata=$1
+cellfile=$2
+platename=$(echo $loaddata | awk -F'[_.]' '{print $1}')
 
-platename=${dir%_JobArray*}
-
-echo Plate_illumAGP.npy > illumAGP.txt
-echo Plate_illumDNA.npy > illumDNA.txt
-echo Plate_illumER.npy > illumER.txt
-echo Plate_illumMito.npy > illumMito.txt
+echo *AGP* > illumAGP.txt
+echo *DNA* > illumDNA.txt
+echo *ER* > illumER.txt
+echo *Mito* > illumMito.txt
 
 pwd > pwd.txt
+cp ${loaddata} .
 
-linecount=$(( $(ls /project/shared/gcrb_igvf/ashley/2021_Chandrasekaran_submitted/2020_11_04_CPJUMP1/images/${platename}*/Images/*.tiff | wc -l) / (8) ))
+linecount=$(awk 'END { print NR -1 }' load_data.csv)
 
-for location in $(cat ../cell_locations.txt); do for i in $(cat illum${location}.txt); do seq 1 ${linecount} | xargs -i -- echo $i > name_illum${location}.txt; done; done
+for location in $(cat ../${cellfile}); do for i in $(cat illum${location}.txt); do seq 1 ${linecount} | xargs -i -- echo $i > filename_illum${location}.txt; done; done
 
 for i in $(cat pwd.txt); do seq 1 ${linecount} | xargs -i -- echo $i > ${platename}_pwd.txt; done
 
 echo FileName_IllumER,PathName_IllumER,FileName_IllumAGP,PathName_IllumAGP,FileName_IllumMito,PathName_IllumMito,FileName_IllumDNA,PathName_IllumDNA > temp.csv
-	
-paste -d, name_illumER.txt ${platename}_pwd.txt name_illumAGP.txt ${platename}_pwd.txt name_illumMito.txt ${platename}_pwd.txt name_illumDNA.txt ${platename}_pwd.txt >> temp.csv
 
-paste -d, /project/shared/gcrb_igvf/ashley/2021_Chandrasekaran_submitted/2020_11_04_CPJUMP1/images/${platename}*/load_data.csv temp.csv > load_data_with_illum.csv
+paste -d, filename_illumER.txt ${platename}_pwd.txt filename_illumAGP.txt ${platename}_pwd.txt filename_illumMito.txt ${platename}_pwd.txt filename_illumDNA.txt ${platename}_pwd.txt >> temp.csv
 
-cd .. 
+paste -d, ${loaddata} temp.csv > ${platename}_load_data_with_illum.csv
