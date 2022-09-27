e04730fa60dca78508ed6aeb5c531bc82ec1a654
ashdederich <ashley.dederich@utsouthwestern.edu>
2022-09-27 12:44:03 -0600

    updated load_data.csv path

--- Preparing_For_CellProfiler/generate_illumCsv.sh
+++ Preparing_For_CellProfiler/generate_illumCsv.sh
@@ -1,8 +1,9 @@
 #!/bin/bash
 
-loaddata=$1
-cellfile=$2
-platename=$(echo $loaddata | awk -F'[_.]' '{print $1}')
+dir=$1
+loaddata=$2
+platename="$(basename $dir)"
+cd ${dir}
 
 echo *AGP* > illumAGP.txt
 echo *DNA* > illumDNA.txt
@@ -10,11 +11,11 @@ echo *ER* > illumER.txt
 echo *Mito* > illumMito.txt
 
 pwd > pwd.txt
-cp ${loaddata} .
+cp ../${loaddata} .
 
 linecount=$(awk 'END { print NR -1 }' load_data.csv)
 
-for location in $(cat ../${cellfile}); do for i in $(cat illum${location}.txt); do seq 1 ${linecount} | xargs -i -- echo $i > filename_illum${location}.txt; done; done
+for location in $(cat ../cell_locations.txt); do for i in $(cat illum${location}.txt); do seq 1 ${linecount} | xargs -i -- echo $i > filename_illum${location}.txt; done; done
 
 for i in $(cat pwd.txt); do seq 1 ${linecount} | xargs -i -- echo $i > ${platename}_pwd.txt; done
 
@@ -22,4 +23,7 @@ echo FileName_IllumER,PathName_IllumER,FileName_IllumAGP,PathName_IllumAGP,FileN
 
 paste -d, filename_illumER.txt ${platename}_pwd.txt filename_illumAGP.txt ${platename}_pwd.txt filename_illumMito.txt ${platename}_pwd.txt filename_illumDNA.txt ${platename}_pwd.txt >> temp.csv
 
-paste -d, ${loaddata} temp.csv > ${platename}_load_data_with_illum.csv
+paste -d, load_data.csv temp.csv > load_data_with_illum.csv
+
+rm *.txt temp.csv
+cd ..
