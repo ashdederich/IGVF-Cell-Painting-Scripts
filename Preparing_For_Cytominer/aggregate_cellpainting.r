ea24cc8cb1c5f021704ae36cf007881132f67066
ashdederich <ashley.dederich@utsouthwestern.edu>
2023-01-06 13:58:08 -0700

    changed aggregate_data() function to aggregate on well mean to keep with literature recommendations

--- Preparing_For_Cytominer/aggregate_cellpainting.r
+++ Preparing_For_Cytominer/aggregate_cellpainting.r
@@ -8,10 +8,13 @@ library(reshape)
 library(tools)
 library(R.utils)
 
+#input file - i.e. IGVFCells.csv.gz
 datafile=args[1]
 
 aggregate_data<-function(input_file){
+    #get the filename
     filename<-sub("\\.csv.gz.*","",input_file)
+    #get the cell location to append to the feature name
     cell_location<-gsub(".*IGVF|.csv.gz*","",input_file)
     input_file=fread(input_file)
     plateid=unique(input_file$Metadata_Plate)
@@ -19,29 +22,30 @@ aggregate_data<-function(input_file){
     file<-input_file[,which(colnames(input_file)=='AreaShape_Area'):ncol(input_file)]
     #add the cellular location to the beginning of the measurement for each input file
     colnames(file)<-paste(cell_location,colnames(file),sep="_")
-    file<-cbind(input_file$Metadata_Plate,input_file$Metadata_Well,file)
-    colnames(file)[1]<-"Metadata_Plate"
-    colnames(file)[2]<-"Metadata_Well"
+    file<-cbind(Metadata_Plate=input_file$Metadata_Plate,Metadata_Well=input_file$Metadata_Well,file)
     
     #find start location of measurements
     col_start<-grep(paste0(cell_location,"_","AreaShape_Area"),colnames(file))
-    #get median and mad and rename the grouping variables
-    median_ofdata=aggregate(file[,col_start:ncol(file)],by=list(file$Metadata_Well),median)
+    #get mean and mad and rename the grouping variables
+    mean_ofdata=aggregate(file[,col_start:ncol(file)],by=list(file$Metadata_Well),mean)
     mad_ofdata=aggregate(file[,col_start:ncol(file)],by=list(file$Metadata_Well),mad)
-    names(median_ofdata)[names(median_ofdata)=="Group.1"]<-"Metadata_Well"
+    names(mean_ofdata)[names(mean_ofdata)=="Group.1"]<-"Metadata_Well"
     names(mad_ofdata)[names(mad_ofdata)=="Group.1"]<-"Metadata_Well"
 
     #add plate info
-    plate<-rep(plateid,nrow(median_ofdata))
-    median_ofdata=cbind(plate,median_ofdata)
+    plate<-rep(plateid,nrow(mean_ofdata))
+    mean_ofdata=cbind(plate,mean_ofdata)
     mad_ofdata=cbind(plate,mad_ofdata)
 
-    median_ofdata=melt(median_ofdata)
+    #reshaping the data frames into long and skinny format
+    mean_ofdata=melt(mean_ofdata)
     mad_ofdata=melt(mad_ofdata)
-    names(median_ofdata)[names(median_ofdata)=="value"]<-"Median"
+    names(mean_ofdata)[names(mean_ofdata)=="value"]<-"Median"
     names(mad_ofdata)[names(mad_ofdata)=="value"]<-"MAD"
 
-    data_med_mad<-merge(median_ofdata,mad_ofdata,by=c("Metadata_Well","variable","plate"))
+    #merging the median and mad data frames by their shared columns
+    data_med_mad<-merge(mean_ofdata,mad_ofdata,by=c("Metadata_Well","variable","plate"))
+    #outputting the file
     write.csv(data_med_mad,gzfile(paste0(filename,"_aggregated",".csv.gz")))
 }
 
