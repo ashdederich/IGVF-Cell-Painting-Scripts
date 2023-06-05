4308e46fd37027369902743eb27d0cdbef1329d7
ashdederich <ashley.dederich@utsouthwestern.edu>
2023-06-05 12:30:41 -0600

    changed col name to Mean

--- Preparing_For_Cytominer/aggregate.r
+++ Preparing_For_Cytominer/aggregate.r
@@ -10,6 +10,7 @@ library(R.utils)
 
 #input file - i.e. IGVFCells.csv.gz
 datafile=args[1]
+#should I add in an argument to read the platemap file and count the number of wells there should be?
 
 aggregate_data<-function(input_file,fileheader,plateid){
     #get the filename
@@ -17,6 +18,8 @@ aggregate_data<-function(input_file,fileheader,plateid){
     #get the cell location to append to the feature name
     cell_location<-gsub(paste0(".*",fileheader,"|.csv.gz*"),"",input_file)
     input_file=fread(input_file,blank.lines.skip=FALSE)
+    wellcount=length(unique(input_file$Metadata_Well)) #set up a qc for wellcount to make sure we retain the correct number of wells
+    #to count number of replicates, read in the platemap file, then exclude the DMSO wells. Then count then number of times each unique value occurs.
     # only keep Metadata_Plate and Metadata_Well and the measurements
     file<-input_file[,which(colnames(input_file)=='AreaShape_Area'):ncol(input_file)]
     #add the cellular location to the beginning of the measurement for each input file
@@ -35,10 +38,15 @@ aggregate_data<-function(input_file,fileheader,plateid){
 
     #reshaping the data frames into long and skinny format
     mean_ofdata=melt(mean_ofdata)
-    names(mean_ofdata)[names(mean_ofdata)=="value"]<-"Median"
+    names(mean_ofdata)[names(mean_ofdata)=="value"]<-"Mean"
+    agg_wellcount<-length(unique(mean_ofdata$Metadata_Well))
+    if(wellcount=agg_wellcount){ # AND num reps are correct AND anything else???{ #should these be nested if statements?
+        #outputting the file
+        write.csv(mean_ofdata,gzfile(paste0(filename,"_aggregated",".csv.gz")))
+    }else{
+        print("After aggregation, there is not the correct number of wells and/or replicates.")
+    }
 
-    #outputting the file
-    write.csv(mean_ofdata,gzfile(paste0(filename,"_aggregated",".csv.gz")))
 }
 
 aggregate_data(datafile,fileheader=args[2],plateid=args[3])
